dafa3c632d0c101cd256e2d6b3469a4d9fa00ed7
JudeH1 <90494861+JudeH1@users.noreply.github.com>
2025-01-27 07:56:03 -0600

    Update Project.tsx

--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -14,21 +14,17 @@ import TextField from '@mui/material/TextField';
 
 
 function createData(
-  name: string,
-  calories: number,
-  fat: number,
-  carbs: number,
-  protein: number,
+  projectName: string,
+  projectLead: string,
+  lastUpdate: string,
+  issues: number,
 ) {
-  return { name, calories, fat, carbs, protein };
+  return { projectName, projectLead, lastUpdate, issues};
 }
 
 const rows = [
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
+  createData('Ticketing System', 'Cuong Ma', 'Jan 21', 42096),
+  createData('Ticketing System', 'Cuong Ma', 'Jan 21', 42096)
 ];
 
 export default function Project() {
@@ -82,26 +78,26 @@ export default function Project() {
       <Table sx={{ minWidth: 1250 }} aria-label="simple table">
         <TableHead>
           <TableRow>
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Project</TableCell>
+            <TableCell align="right">Project Lead</TableCell>
+            <TableCell align="right">Last Issue Update</TableCell>
+            <TableCell align="right">Issues</TableCell>
+            <TableCell align="right">Actions</TableCell>
           </TableRow>
         </TableHead>
         <TableBody>
           {rows.map((row) => (
             <TableRow
-              key={row.name}
+              key={row.projectName}
               sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
             >
               <TableCell component="th" scope="row">
-                {row.name}
+                {row.projectName}
               </TableCell>
-              <TableCell align="right">{row.calories}</TableCell>
-              <TableCell align="right">{row.fat}</TableCell>
-              <TableCell align="right">{row.carbs}</TableCell>
-              <TableCell align="right">{row.protein}</TableCell>
+              <TableCell align="right">{row.projectLead}</TableCell>
+              <TableCell align="right">{row.lastUpdate}</TableCell>
+              <TableCell align="right">{row.issues}</TableCell>
+
             </TableRow>
           ))}
         </TableBody>
